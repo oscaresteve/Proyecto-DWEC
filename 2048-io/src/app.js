b04248113f7438aa2df941513084b47a9e55c8b7
oscaresteve <oscarestevea24@gmail.com>
2025-12-09 11:12:38 +0100

    escuchar solo cambios de ruta

--- 2048-io/src/app.js
+++ 2048-io/src/app.js
@@ -4,16 +4,21 @@ import { renderLoginView } from './views/loginView.js';
 import { renderRegisterView } from './views/registerView.js';
 import { renderGameView } from './views/gameView.js';
 
+let currentRoute = null;
+
 export function initApp() {
   router.init();
 
-state$.subscribe(state => {
-  const route = state.route;
-  const root = document.getElementById('app');
-  root.innerHTML = '';
+  state$.subscribe(state => {
+    const route = state.route;
+    const root = document.getElementById('app');
 
-  if (route === 'login') renderLoginView(root);
-  else if (route === 'register') renderRegisterView(root);
-  else if (route === 'game') renderGameView(root);
-});
+    if (route !== currentRoute) {
+      root.innerHTML = '';
+      if (route === 'login') renderLoginView(root);
+      else if (route === 'register') renderRegisterView(root);
+      else if (route === 'game') renderGameView(root);
+      currentRoute = route;
+    }
+  });
 }
